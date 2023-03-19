b1a294b7985615a98a3940c672e999a58b8a7f91
MiksKristiansLatiss <125253937+MiksKristiansLatiss@users.noreply.github.com>
2023-03-20 01:09:08 +0200

    Test2

--- build_heap.py
+++ build_heap.py
@@ -1,12 +1,12 @@
 # python3
 
 
-def build_heap(data):
+def build_heap(n, data):
     swap_count = 0
     swap_list = []
     size = n - 1
 
-def sift_down(i):
+    def sift_down(i):
         nonlocal swap_count, swap_list, data
 
         min_index = i
@@ -28,33 +28,16 @@ def sift_down(i):
     for i in range(n // 2, -1, -1):
         sift_down(i)
 
-
     return swap_count, swap_list
 
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
     n = int(input())
     data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
     assert len(data) == n
 
-    # calls function to assess the data 
-    # and give back all swaps
-    swaps = build_heap(data)
+    swap_count, swap_list = build_heap(n, data)
 
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(swap_count)
     for swap in swap_list:
         print(swap[0], swap[1])
