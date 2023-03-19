bff4bd9a0a32c9420458a5672ff1b15414d061d7
MiksKristiansLatiss <125253937+MiksKristiansLatiss@users.noreply.github.com>
2023-03-20 01:06:10 +0200

    Test

--- build_heap.py
+++ build_heap.py
@@ -2,12 +2,34 @@
 
 
 def build_heap(data):
-    swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
+    swap_count = 0
+    swap_list = []
+    size = n - 1
+
+def sift_down(i):
+        nonlocal swap_count, swap_list, data
+
+        min_index = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+
+        if left <= size and data[left] < data[min_index]:
+            min_index = left
+
+        if right <= size and data[right] < data[min_index]:
+            min_index = right
+
+        if i != min_index:
+            data[i], data[min_index] = data[min_index], data[i]
+            swap_count += 1
+            swap_list.append((i, min_index))
+            sift_down(min_index)
+
+    for i in range(n // 2, -1, -1):
+        sift_down(i)
 
 
-    return swaps
+    return swap_count, swap_list
 
 
 def main():
@@ -33,9 +55,9 @@ def main():
 
 
     # output all swaps
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
+    print(swap_count)
+    for swap in swap_list:
+        print(swap[0], swap[1])
 
 
 if __name__ == "__main__":
